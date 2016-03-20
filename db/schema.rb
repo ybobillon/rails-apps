251b93a60b5b1657b2793cb3de3a0b70639b0d60
ybobillon <bobillon.yoann@gmail.com>
2016-03-20 14:33:19 +0100

    Fin de l'application demo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160320124730) do
+ActiveRecord::Schema.define(:version => 20160320131132) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "nom"
